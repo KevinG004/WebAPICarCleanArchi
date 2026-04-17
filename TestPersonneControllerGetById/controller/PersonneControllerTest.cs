e1c9c8f7bb81be23dca59494d2e555c29961e7b6
shrek206 <kevin.grignard.pro@gmail.com>
2026-04-17 16:47:23 +0200

    test pipeline

---
+++ TestPersonneControllerGetById/controller/PersonneControllerTest.cs
@@ -0,0 +1,67 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using CarApiClean.Controllers;
+using CarList.Core.Interfaces.Services.Data;
+using CarList.Core.DTOs.Responses;
+
+namespace TestPersonneControllerGetById.controller
+{
+    public class PersonneControllerTest
+    {
+        private readonly Mock<IPersonneService> _mockService;
+        private readonly PersonnesController _controller;
+
+        public PersonneControllerTest()
+        {
+            _mockService = new Mock<IPersonneService>();
+            _controller = new PersonnesController(_mockService.Object);
+        }
+
+        [Fact]
+        public async Task GetById_ReturnsOk_WhenPersonneExists()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var fakePersonne = new PersonneAddResponseDTO
+            {
+                Email = "test@test.com",
+                FirstName = "John",
+                Password = "password123",
+                LastName = "Doe",
+                Cars = null
+            };
+
+            _mockService
+                .Setup(s => s.GetById(id))
+                .ReturnsAsync(fakePersonne);
+
+            // Act
+            var result = await _controller.Get(id);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            Assert.Equal(fakePersonne, okResult.Value);
+        }
+
+        [Fact]
+        public async Task GetById_ReturnsNotFound_WhenPersonneDoesNotExist()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+
+            _mockService
+                .Setup(s => s.GetById(id))
+                .ReturnsAsync((PersonneAddResponseDTO?)null);
+
+            // Act
+            var result = await _controller.Get(id);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result.Result);
+        }
+    }
+}
